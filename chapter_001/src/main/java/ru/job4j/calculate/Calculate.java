c042a4733ae41116491f96599d4561586c1ee8d5
Denis Abakumov <denisab85@gmail.com>
2019-02-08 20:51:06 -0600

    1.3. JavaDoc. Code convention[#111298]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ * The home of a 'Hello World' method
+ *
+ * @author Denis Abakumov
+ */
 public class Calculate {
 	
+   /**
+	* Main method, the app's entry point
+	* 
+	* @param args - CLI arguments. Not used
+	*/
 	public static void main (String[] args){
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Denis Abakumov (denisab85@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
