e2ef4b6e3be794626c3e48e09ddbbc4c6b8c8381
Denis Abakumov <denisab85@gmail.com>
2019-02-14 22:10:53 -0600

    5.5. Рефакторинг кода.[#111267]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 import java.util.StringJoiner;
+import java.util.function.BiPredicate;
 
 /**
  * Paints different geometric shapes
@@ -16,15 +17,19 @@ public class Paint {
      * @return string representing a pyramid.
      */
     public String pyramid(int height) {
+        return this.loopBy(height * 2 - 1, height, (x, y) -> y >= height - x - 1 && y + height - 1 >= x);
+    }
+
+    private String loopBy(int width, int height, BiPredicate<Integer, Integer> predicate) {
         StringJoiner board = new StringJoiner(System.lineSeparator());
-        for (int y = height - 1; y >= 0; y--) {
+        for (int y = 0; y < height; y++) {
             StringBuilder row = new StringBuilder();
-            for (int x = 0; x < height * 2 - 1; x++) {
-                boolean print = (x >= y) && (x <= height * 2 - 2 - y);
-                row.append(print ? "^" : " ");
+            for (int x = 0; x < width; x++) {
+                row.append(predicate.test(x, y) ? "^" : " ");
             }
             board.add(row.toString());
         }
         return board.toString();
     }
+
 }
