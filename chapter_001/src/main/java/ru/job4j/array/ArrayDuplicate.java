0a2644f2d3e782d3f1dd9160c21a97f983fcdccd
Denis Abakumov <denisab85@gmail.com>
2019-02-18 18:06:33 -0600

    6.8. Удаление дубликатов в массиве.[#111282]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * Removes duplicate elements from a String array.
+ *
+ * @author Denis Abakumov
+ * @version $Id$
+ */
+public class ArrayDuplicate {
+
+    /**
+     * Removes duplicate elements from a String array.
+     * Note: will also sort elements in the input array leaving all duplicates at the end.
+     *
+     * @param array array to remove duplicates from.
+     * @return a new array without duplicate elements.
+     */
+    public String[] remove(String[] array) {
+        int end = array.length;
+        int i = 0;
+        while (++i < end) {
+            // compare with each of the preceding elements
+            for (int c = 0; c < i; c++) {
+                if (array[i].equals(array[c])) {
+                    // move current element #i to the end
+                    String duplicate = array[i];
+                    System.arraycopy(array, i + 1, array, i, array.length - i - 1);
+                    array[--end] = duplicate;
+                }
+            }
+        }
+        return Arrays.copyOf(array, end);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Denis Abakumov
+ * @version $Id$
+ */
+public class ArrayDuplicateTest {
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        String[] array = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] expected = {"Привет", "Мир", "Супер"};
+
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] actual = duplicate.remove(array);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenNoDuplicatesThenArrayUnchanged() {
+        String[] array = {"Привет", "Супер", "Мир"};
+        String[] expected = {"Привет", "Супер", "Мир"};
+
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] actual = duplicate.remove(array);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenAllDuplicatesThenOneStringArray() {
+        String[] array = {"Привет", "Привет"};
+        String[] expected = {"Привет"};
+
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] actual = duplicate.remove(array);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenOneElementThenNoChange() {
+        String[] array = {"Привет"};
+        String[] expected = {"Привет"};
+
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] actual = duplicate.remove(array);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenNoElementsThenNoChange() {
+        String[] array = {};
+        String[] expected = {};
+
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] actual = duplicate.remove(array);
+
+        assertThat(actual, is(expected));
+    }
+}
