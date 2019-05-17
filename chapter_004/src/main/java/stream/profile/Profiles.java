cdc4097ad8355b943839888bd40b02a085641650
Denis Abakumov <denisab85@gmail.com>
2019-05-16 19:33:48 -0500

    3. Уникальность элементов и сортировка.[#111228] - fix

--- chapter_004/src/main/java/stream/profile/Profiles.java
+++ chapter_004/src/main/java/stream/profile/Profiles.java
@@ -6,6 +6,6 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).distinct().sorted().collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).sorted().distinct().collect(Collectors.toList());
     }
 }
